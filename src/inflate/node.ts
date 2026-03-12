f2e01f2c9183fa5fd3758bf111f230641354cab3
laplace-anon <150580554+laplace-anon@users.noreply.github.com>
2026-03-12 20:33:33 +0800

    refactor: remove unnecessary type assertion in async inflate functions

--- src/inflate/node.ts
+++ src/inflate/node.ts
@@ -4,7 +4,7 @@ import { brotliDecompress, inflate } from 'node:zlib'
 const _inflate = promisify(inflate)
 const _brotli = promisify(brotliDecompress)
 
-const inflateAsync = (b: Uint8Array) => _inflate(b) as Promise<Uint8Array>
-const brotliDecompressAsync = (b: Uint8Array) => _brotli(b) as Promise<Uint8Array>
+const inflateAsync = (b: Uint8Array) => _inflate(b)
+const brotliDecompressAsync = (b: Uint8Array) => _brotli(b)
 
 export const inflates = { inflateAsync, brotliDecompressAsync }
